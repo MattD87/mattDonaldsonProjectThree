ad2a9ded2aaa649016a79d6000427ba3efd2cc54
Matt Donaldson <matthewdonaldson9@gmail.com>
2019-08-20 15:35:31 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,41 @@
+const quizApp = {};
+quizApp.score = 0;
+quizApp.categories = {
+  history: {
+    200: "200$ question",
+    400: "400$ question",
+    600: "600$ question",
+    800: "800$ question",
+    1000: "1000$ question"
+  },
+  geography: {
+    200: "200$ question",
+    400: "400$ question",
+    600: "600$ question",
+    800: "800$ question",
+    1000: "1000$ question"
+  },
+  popCulture: {
+    200: "200$ question",
+    400: "400$ question",
+    600: "600$ question",
+    800: "800$ question",
+    1000: "1000$ question"
+  },
+  lethalDrinkables: {
+    200: "200$ question",
+    400: "400$ question",
+    600: "600$ question",
+    800: "800$ question",
+    1000: "1000$ question"
+  }
+};
+
+quizApp.init = function() {
+
+}
+
+$(function() {
+  quizApp.init();
+});
+
