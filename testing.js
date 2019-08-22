442fb170c9ec9d921061a8f762d8882b31b4d93e
Matt Donaldson <matthewdonaldson9@gmail.com>
2019-08-22 19:03:20 -0400

    continuing work on javascript functionality

--- testing.js
+++ testing.js
@@ -7,35 +7,35 @@ quizApp.score = 0;
 quizApp.questions = [
   //category 1 questions
   {
-    title: `200$ Question`,
+    title: `Cat 1 200$ Question`,
     answers: [`answer 1`, `answer 2`, `answer 3`, `answer 4`],
     correct: 1,
     cash: 200,
     id: 1200
   },
   {
-    title: `$400 question`,
+    title: `Cat 1 $400 question`,
     answers: [`answer 1`, `answer 2`, `answer 3`, `answer 4`],
     correct: 1,
     cash: 400,
     id: 1400
   },
   {
-    title: `$600 question`,
+    title: `Cat 1 $600 question`,
     answers: [`answer 1`, `answer 2`, `answer 3`, `answer 4`],
     correct: 1,
     cash: 600,
     id: 1600
   },
   {
-    title: `$800 question`,
+    title: `Cat 1 $800 question`,
     answers: [`answer 1`, `answer 2`, `answer 3`, `answer 4`],
     correct: 1,
     cash: 800,
     id: 1800
   },
   {
-    title: `$1000 question`,
+    title: `Cat 1 $1000 question`,
     answers: [`answer 1`, `answer 2`, `answer 3`, `answer 4`],
     correct: 1,
     cash: 1000,
@@ -43,7 +43,7 @@ quizApp.questions = [
   },
   //category 2 questions
   {
-    title: `200$ Question`,
+    title: `Cat 2 200$ Question`,
     answers: [`answer 1`, `answer 2`, `answer 3`, `answer 4`],
     correct: 1,
     cash: 200,
@@ -79,7 +79,7 @@ quizApp.questions = [
   },
   //category 3 questions
   {
-    title: `200$ Question`,
+    title: `Cat 3 200$ Question`,
     answers: [`answer 1`, `answer 2`, `answer 3`, `answer 4`],
     correct: 1,
     cash: 200,
@@ -115,7 +115,7 @@ quizApp.questions = [
   },
   //category 4 questions
   {
-    title: `200$ Question`,
+    title: `Cat 4 200$ Question`,
     answers: [`answer 1`, `answer 2`, `answer 3`, `answer 4`],
     correct: 1,
     cash: 200,
@@ -160,7 +160,7 @@ quizApp.startQuiz = function() {
   });
 };
 quizApp.showQuestion = function() {
-  $("button").on("click", function() {
+  $(".button").on("click", function() {
     const buttonId = $(this).attr("id");
     // console.log(buttonId);
     quizApp.questionContent(buttonId);
@@ -168,36 +168,41 @@ quizApp.showQuestion = function() {
   });
 };
 quizApp.growQuestion = function() {
-  $("button").on("click", function() {
-    $(this).parent().toggleClass("change-size");
+  $(".button").on("click", function() {
+    $('.questionContainer').toggleClass("change-size");
   });
 };
 
 quizApp.questionContent = (clickId) => {
-  //  const question = questions[id];
-   console.log(clickId);
-   console.log(quizApp.questions);
+                                    //for each item in quizapp.questions
+   for (let i = 0; i < quizApp.questions.length; i++ ){    
+                                  // if click id is same as array id                                  
+    if (clickId == quizApp.questions[i].id) {
+                                    //save that objects title and answers in two variables
+      const question = quizApp.questions[i].title;
+      const answers = quizApp.questions[i].answers;
+                                  //grab the box with the click id and display the title and answers
+      $('.questionContainer').html(`<h2>${question}</h2><ul class="questions"></ul>`);
+      for (let i = 0; i < answers.length; i++) {
+        $('.questions').append(`<li>${answers[i]}</li>`);        
+      }
+    };
+  };
+};
+
+quizApp.selectOption = function() {
+  $(`.questionContainer`).on(`click`, `li`, function() {
+    $(`.selected`).removeClass(`selected`);
+    $(this).addClass(`selected`);
+  });
 }
-
-// quizApp.questionContent = (questions) => {
-//   quizApp.questions.forEach((set) => {
-//     const listItem = $("<ul>");
-//     const title = $("<h2>").text(set.title);
-//     const answers = $("li").text(set.answers);
-
-//     listItem.append(title, answers)
-//     $(".display").append(listItem); 
-//   });
-//   console.log(questionContent);
-// }
-
 
 //initialization
 quizApp.init = function() {
   this.startQuiz();
   this.showQuestion();
   this.growQuestion();
-  // this.questionContent();
+  this.selectOption();
 };
 
 //document ready
